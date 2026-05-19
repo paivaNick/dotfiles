c13767c9f025dbd711a1e4ef350873a7d2cb050f
nicolas paiva <nipaiva93@gmail.com>
2026-05-19 00:44:49 -0300

    adicionando o plugin de ssh para o fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
 	starship init fish | source 
-  sleep 0.1
-    set -U fish_greeting "🐧 Olá, Nicolas! 🚀"
+  fish_ssh_agent
+  set -U fish_greeting "🐧 Olá, Nicolas! 🚀"
 end
